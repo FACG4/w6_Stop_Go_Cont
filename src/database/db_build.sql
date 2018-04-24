85f73d07c00c7f815a761a51d1c8b3ce7b94df31
ishak <ishak-3@hotmail.com>
2018-04-24 16:26:51 +0300

    check user data

--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -3,6 +3,7 @@ DROP TABLE IF EXISTS users, posts, comments CASCADE;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY ,
+  name VARCHAR NOT NULL ,
   user_name VARCHAR UNIQUE NOT NULL ,
   password VARCHAR NOT NULL CHECK (length(password)>=6) ,
   role VARCHAR DEFAULT 'student',
@@ -11,23 +12,23 @@ CREATE TABLE users (
 
 CREATE TABLE posts (
   id SERIAL PRIMARY KEY,
-  user_id  INTEGER REFERENCES users(id) ON UPDATE CASCADE,
+  user_id  INTEGER REFERENCES users(id) ON DELETE CASCADE,
   post_content TEXT NOT NULL CHECK (length(post_content)>0),
   type_of_note  VARCHAR(10) NOT NULL CHECK (type_of_note IN ('STOP', 'GO', 'CONT'))
 );
 
 CREATE TABLE comments (
   id SERIAL PRIMARY KEY,
-  post_id INTEGER REFERENCES posts(id) ON UPDATE CASCADE,
-  user_id INTEGER REFERENCES users(id) ON UPDATE CASCADE,
+  post_id INTEGER REFERENCES posts(id) ON DELETE CASCADE,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   comment_content TEXT NOT NULL
 );
 
-INSERT INTO users (user_name,password,role,email)
-VALUES ('MARWA','123456',DEFAULT,'ma@mail.com'),
-        ('MOHAMMED','123456',DEFAULT, 'mo@email.com'),
-        ('ISHAK','123456',DEFAULT,'i@mail.com'),
-        ('RAMY','123456',DEFAULT,'r@mail.com');
+INSERT INTO users (name,user_name,password,role,email)
+VALUES ('MARWA','MARWA','123456',DEFAULT,'ma@mail.com'),
+        ('MOHAMMED','MOHAMMED','123456',DEFAULT, 'mo@email.com'),
+        ('ISHAK','ISHAK','123456',DEFAULT,'i@mail.com'),
+        ('RAMY','RAMY','123456',DEFAULT,'r@mail.com');
 
 INSERT INTO posts (user_id,post_content,type_of_note)
 VALUES (1,'stop stop stop','STOP'),
